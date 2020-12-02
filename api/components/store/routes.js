bec98712eadc0f30626ebe7aebb9edb9a9955e46
Lucas Wilker <lwilker24@gmail.com>
2020-12-01 21:07:47 -0300

    Create store routes

---
+++ api/components/store/routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const auth = require('../../routes/auth');
+const StoreController = require('../store/controller')
+const storeController = new StoreController();
+
+router.get('/', storeController.index);
+router.get('/:id', storeController.getStoreById);
+
+router.post('/', auth.required, storeController.store);
+router.put('/:id', auth.required, storeController.update);
+router.delete('/:id', auth.required, storeController.remove);
+
+
+module.exports = router;
