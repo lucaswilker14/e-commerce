9240a0030bc52fb1c018bb4bc196dc1cd1628ae0
Lucas Wilker <lwilker24@gmail.com>
2020-12-01 21:10:11 -0300

    Importing routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('../components/user/routes')
+const userRoutes = require('../components/user/routes');
+const storeRoutes = require('../components/store/routes');
 
 router.use('/usuario', userRoutes);
+router.use('/loja', storeRoutes);
 
 module.exports = router;
