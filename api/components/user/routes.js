422385849012223c61a3e18459d8958a9b68ee0e
Lucas Wilker <lwilker24@gmail.com>
2020-12-03 00:02:49 -0300

    Added new validator in the user routes

--- api/components/user/routes.js
+++ api/components/user/routes.js
@@ -1,26 +1,36 @@
 import { Router } from 'express';
 import auth  from '../../auth/auth';
-import UserController from '../user/controller'
+import {  getByIdValidator
+        , registerUserValidator
+        , loginValidator
+        , updateUserValidator
+        , removeUserValidator
+        , createRevoveryValidator
+        , showFinishRecoveryValidator
+        , finishRecoveryValidator } from './userValidation';
+import { createValidator } from 'express-joi-validation';
+import UserController from '../user/controller';
 
 const router = Router();
+const validator = createValidator({passError: true});
 const userController = new UserController();
 
 
-router.post('/cadastro', userController.registerUser);
-router.post('/login', userController.login);
+router.post('/cadastro', validator.body(registerUserValidator), userController.registerUser);
+router.post('/login', validator.body(loginValidator), userController.login);
 
-router.put('/', auth.required, userController.update)
+router.put('/', auth.required, validator.body(updateUserValidator), userController.update)
 
-router.delete('/', auth.required, userController.removeUserAccount);
+router.delete('/', auth.required, validator.params(removeUserValidator), userController.removeUserAccount);
 
 // password recovery
 router.get('/recuperar-senha', userController.showRecovery);
-router.post('/recuperar-senha', userController.createRecovery);
-router.get('/senha-recuperada', userController.showFinishRecovery)
-router.post('/senha-recuperada', userController.finishRecovery);
+router.post('/recuperar-senha', validator.body(createRevoveryValidator), userController.createRecovery);
+router.get('/senha-recuperada', validator.query(showFinishRecoveryValidator), userController.showFinishRecovery)
+router.post('/senha-recuperada', validator.body(finishRecoveryValidator), userController.finishRecovery);
 
 router.get('/', auth.required, userController.index);
-router.get('/:id', auth.required, userController.getUserById);
+router.get('/:id', auth.required, validator.params(getByIdValidator), userController.getUserById);
 
 
 module.exports = router;
