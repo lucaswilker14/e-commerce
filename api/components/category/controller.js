d1fbfadd566c1e7c1355f5dc69b63f45be78444f
Lucas Wilker <lwilker24@gmail.com>
2020-12-23 17:46:38 -0300

    base controller

--- api/components/category/controller.js
+++ api/components/category/controller.js
@@ -1,6 +1,57 @@
+import { model } from 'mongoose'
+const categoryModel = model('Category');
+
 
 class CategoryController {
 
+    async index(req, res, next) {
+        try {
+            const category = await categoryModel.find({ store: req.query.loja }).select('_id product name code store');
+            return res.send({categories: category}).status('200');
+        } catch (e) {
+            next(e)
+        }
+    };
+
+    async getAvailability(req, res, next) {
+        try {
+
+        } catch (e) {
+            next(e)
+        }
+    };
+
+    async getCategoryByID(req, res, next) {
+        try {
+
+        } catch (e) {
+            next(e)
+        }
+    };
+
+    async createCategory(req, res, next) {
+        try {
+
+        } catch (e) {
+            next(e)
+        }
+    };
+
+    async update(req, res, next) {
+        try {
+
+        } catch (e) {
+            next(e)
+        }
+    };
+
+    async delete(req, res, next) {
+        try {
+
+        } catch (e) {
+            next(e)
+        }
+    };
 }
 
 module.exports = CategoryController;
