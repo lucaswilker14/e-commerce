adc7c7589488b68c82b33fdab6ffb95b6dc5e866
Lucas Wilker <lwilker24@gmail.com>
2020-12-29 21:03:12 -0300

    Added routes validations

--- api/components/category/routes.js
+++ api/components/category/routes.js
@@ -3,19 +3,22 @@ import CategoryController from './controller';
 import { isAdmin } from '../store/storeValidation'
 import { createValidator } from "express-joi-validation";
 import auth from '../../auth/auth';
+import { index, getAvailable, getAvailableByID, createCategory, update, remove } from './categoryValidation';
 
 const router                = Router();
 const categoryController    = new CategoryController();
 const validator             = createValidator({passError: true});
 
-router.get('/', categoryController.index);
-router.get('/disponiveis', categoryController.getAvailable);
-router.get('/:id', categoryController.getCategoryByID);
+router.get('/', validator.query(index), categoryController.index);
+router.get('/disponiveis', validator.query(getAvailable), categoryController.getAvailable);
+router.get('/:id', validator.query(getAvailableByID.query), validator.params(getAvailableByID.params),
+    categoryController.getCategoryByID);
 
-router.post('/', auth.required, isAdmin, categoryController.createCategory);
+router.post('/', auth.required, isAdmin, validator.body(createCategory), categoryController.createCategory);
 
-router.put('/:id', auth.required, isAdmin, categoryController.update);
+router.put('/:id', auth.required, isAdmin, validator.body(update.body), validator.params(update.params),
+    categoryController.update);
 
-router.delete('/:id', auth.required, isAdmin, categoryController.delete);
+router.delete('/:id', auth.required, isAdmin, validator.params(remove), categoryController.remove);
 
 module.exports = router;
